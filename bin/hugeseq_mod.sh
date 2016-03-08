3b4d1c126297858569d4062d8868f573c63ed995
Hannes Roest <hroest@stanford.edu>
2016-03-07 22:15:21 -0800

    bring down memory usage

--- bin/hugeseq_mod.sh
+++ bin/hugeseq_mod.sh
@@ -3,6 +3,8 @@
 module load hugeseq/2.0
 # make Hugeseq work even when not in default location
 export PATH=$HUGESEQ_HOME/bin/:$PATH
+# bring down the memory usage, leads to fewer killed jobs
+export MALLOC_ARENA_MAX=2
 export TMP=$1
 echo $TMP
 shift
